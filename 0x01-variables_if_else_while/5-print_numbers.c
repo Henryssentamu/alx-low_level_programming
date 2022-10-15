a069d977b79fc429d48b8f7ab6a3e4be661cd786
Henryssentamu <ssentamuhenry61@gmail.com>
2022-10-15 14:06:00 -0700

    program to print single numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main -a program to print single numbers from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	int k;
+
+	i = 0;
+
+	for (k = 0; k < 10; k++)
+	{
+		printf("%d", i);
+
+		i++;
+
+	}
+	printf("\n");
+
+	return (0);
+}
