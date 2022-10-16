9dae1841f0e8cf2f99cca097182418b0b25de6dd
Henryssentamu <ssentamuhenry61@gmail.com>
2022-10-16 06:00:50 -0700

    update

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,11 +7,11 @@ int main(void)
 {
 	int a, b, c;
 
-	for (a = '0'; a < '9'; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		for (b = '0'; b < '9'; b++)
+		for (b = '0'; b <= '9'; b++)
 		{
-			for (c = '0'; c < '9'; c++)
+			for (c = '0'; c <= '9'; c++)
 			{
 				if (a < b && b < c)
 				{
