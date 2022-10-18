7d52db6c46fc48dd1b1da72b25deb2255f84ad40
Henryssentamu <ssentamuhenry61@gmail.com>
2022-10-18 07:30:33 -0700

    function to print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet -> prints all lowercase alphabets
+ */
+void print_alphabet(void);
+{
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	putchar('\n');
+
