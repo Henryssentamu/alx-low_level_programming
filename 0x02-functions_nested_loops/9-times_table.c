43c27e00a6a4fb3c5533dbf7b67053d58b4ed57d
Henryssentamu <ssentamuhenry61@gmail.com>
2022-10-19 04:45:39 -0700

    multiple of nine

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * times_table -> the 9 times table
+ */
+void times_table(void)
+{
+	int a, b, c;
+
+	for (b = 0; b < 9; b++)
+	{
+		_putchar('0');
+
+		_putchar(',');
+
+		_putchar(' ');
+
+
+		for (a = 0; a < 9; a++)
+		{
+			c = a * b;
+
+			if ((c / 10) > 0)
+			{
+				_putchar((c / 10) + '0');
+			}
+
+			else
+			{
+				_putchar(' ');
+			}
+
+			_putchar((c % 10) + '0');
+
+			if (a < 9)
+			{
+				_putchar(',');
+
+				_putchar(' ');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
