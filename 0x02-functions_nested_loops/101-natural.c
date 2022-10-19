30b896415f022aec6791a7130967a3d03d5ba0d1
Henryssentamu <ssentamuhenry61@gmail.com>
2022-10-19 06:59:55 -0700

    number

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return:0
+ */
+int main(void)
+{
+
+	int a, sum;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 15) == 0)
+		{
+			sum += 1;
+		}
+	}
+	printf("%d", sum);
+
+	return (0);
+}
