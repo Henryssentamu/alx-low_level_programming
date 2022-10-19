b2770a7b1315e0056f512dff2cb1c85cbf35192b
Henryssentamu <ssentamuhenry61@gmail.com>
2022-10-19 00:15:11 -0700

    fixed

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  *  _islower -> checking for lowercase in alphabet
- *  Return: 1 and 0 where c is and integer
+ *  Return: 1 and 0 
+ *  parameter: c
  */
 int _islower(int c)
 {
