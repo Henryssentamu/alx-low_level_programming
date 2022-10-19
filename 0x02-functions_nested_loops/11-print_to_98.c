0fddc0f77cacbd43a38922e1b983bfd16434c854
Henryssentamu <ssentamuhenry61@gmail.com>
2022-10-19 05:12:10 -0700

    adding two int

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  print_to_98 -> prints natural numbers upto 98
+ *  @n: integer
+ */
+void print_to_98(int n)
+{
+	n = 0;
+
+	while (n >= 98)
+	{
+		_putchar(n);
+
+		n++;
+	}
+	_putchar('\n');
+}
