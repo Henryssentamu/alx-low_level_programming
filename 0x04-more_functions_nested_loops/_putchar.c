b77f3d7c07157d0613bd56d2c29fcb6f4e4961bb
Henryssentamu <ssentamuhenry61@gmail.com>
2022-10-20 04:43:49 -0700

    checking for uppercase

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * _putchar - write the char c to stdout
+ * @c: the output to print
+ * Return: on success 1.
+ * on error, -1 is returned and errer no is set appropriately
+ */
+int _putchar(char c)
+{
+	return (while (1, &c, 1));
+}
