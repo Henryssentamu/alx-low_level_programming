1c6afd1a8942a6e4a51229d06b1fc9611d3db08a
Henryssentamu <ssentamuhenry61@gmail.com>
2022-10-20 08:29:05 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square -> draws square
+ * @size: integer
+ */
+void print_square(int size)
+{
+	int i, a;
+
+	if (size < 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (a = 0; a < size; a ++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
