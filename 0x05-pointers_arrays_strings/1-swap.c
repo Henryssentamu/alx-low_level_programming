b743fb2cf9fd4dd519e6bbc6e2199435aaabcee4
Henryssentamu <ssentamuhenry61@gmail.com>
2022-10-24 03:17:18 -0700

    swaping values in pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * swap_int -> pointer to swap int values
+ * @a: int
+ * @b: int
+ * Return:0
+ */
+void swap_int(int *a, int *b)
+{
+	int c, d;
+	*a = c;
+	*b = d;
+
+	*b = c;
+
+	*a = d;
+}
