4f94245ffa10ff75030a540c5a59f0a4889ed623
Henryssentamu <ssentamuhenry61@gmail.com>
2022-10-27 01:07:01 -0700

    updated

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,21 +4,23 @@
  * @n:pointer
  * Return: n
  */
-char *leet(char *)
+char *leet(char *n)
 {
 	int i, j;
 
-	char l1[] = "aAeEoOtTlL";
+	int find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 
-	char l2[] = "4433007711";
+	int replacer[] = {'4', '3', '0', '7', '1'};
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			if (n[i] == l1[j])
+			if (n[i] == find[j])
 			{
-				n[i] = l2[j];
+				n[i] = replacer[j / 2];
+
+				j = 9;
 			}
 		}
 	}
