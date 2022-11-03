cf41f964117198dd078708102498794d8029b3c0
Henryssentamu <ssentamuhenry61@gmail.com>
2022-11-03 03:22:16 -0700

    prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * is_prime_number -> print a prime number
+ * @n: int as argument
+ * Return: int
+ */
+int is_prime_number(int n)
+{
+	if (n / 1 != n && n % 2 == 0)
+		return (0);
+	return (1);
+	is_prime_number(n);
+}
