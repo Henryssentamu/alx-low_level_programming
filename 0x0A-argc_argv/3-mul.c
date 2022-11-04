9ca7d60d74a5f174d22ce590dd8a2668de5376f7
Henryssentamu <ssentamuhenry61@gmail.com>
2022-11-04 04:29:51 -0700

    fixed

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,18 +9,18 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 1;
+	int i, mult = 1;
 
 	if (argc == 1 || argc == 2)
 	{
-		printf(" Error \n");
-
+		printf("Error\n");
 		return (1);
 	}
-	for (i = 0; i < argc; i++)
+	else
 	{
-		sum *= atoi(argv[i]);
+		for (i = 1; i < 3; i++)
+			mult *= atoi(argv[i]);
+		printf("%d", mult);
 	}
-	printf("sum = %d\n", sum);
 	return (0);
 }
