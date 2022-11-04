c3b721d525cf51a18bd7f3174423bed5f33357e0
Henryssentamu <ssentamuhenry61@gmail.com>
2022-11-04 02:29:29 -0700

    prints argc and argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main -> prints the number of arguments passed into it
+ * @argc: number of arguments
+ * @argv: pointer of array
+ * Return: integer
+ */
+int main(int argc, char * argv[])
+{
+	int i;
+	
+	for (i = 0; i < argc; i++)
+		printf("number of argument = %d", i);
+	return (0);
+}
