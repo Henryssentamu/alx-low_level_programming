9e3318c286935894d8e3e2769bb891d54da748ab
Henryssentamu <ssentamuhenry61@gmail.com>
2022-11-04 04:23:00 -0700

    fixed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,7 +8,15 @@
  */
 int main(int argc, char *argv[])
 {
-	printf("number of arguments = %d\n", argc);
+	int i;
 
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++)
+			;
+		printf("%d\n", i - 1);
+	}
 	return (0);
 }
