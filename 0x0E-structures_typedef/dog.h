b6d8c80ee228fc8d97e321fc25d5822dc7b5a0e6
Henryssentamu <ssentamuhenry61@gmail.com>
2022-11-14 00:44:14 -0800

    structures in c

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct dog - data type
+ * @name: string
+ * @age: float
+ * owner:string
+ * main -> returns int & no arg
+ * Reurn : integer
+ */
+int main(void)
+{
+	struct dog
+	{
+		char *name;
+
+		float age;
+
+		char *owner;
+	};
+	return (0);
+}
