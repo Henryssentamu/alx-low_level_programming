7876b1b3a6abf0e14ccaad5abcf47e20f14155ed
Henryssentamu <ssentamuhenry61@gmail.com>
2022-11-23 00:03:39 -0800

    fixed

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -19,9 +19,12 @@ int sum_them_all(const unsigned int n, ...)
 
 	if (n == 0)
 		return (0);
-	for (i = 0; i < n; i++)
+	if ( n > 0 )
 	{
-		sum += va_arg(ptr, int);
+		for (i = 0; i < n; i++)
+		{
+			sum += va_arg(ptr, int);
+		}
 	}
 	return (sum);
 }
