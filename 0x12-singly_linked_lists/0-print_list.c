8fd9cb28edccce7ded64028e9bb463f1c8e78c69
Henryssentamu <ssentamuhenry61@gmail.com>
2022-11-30 02:40:20 -0800

    printing in single-linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,20 +8,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t node  = 0;
+	size_t nodes = 0;
 
-	if (h->str == NULL)
-		printf("0");
-	else
+	while (h)
 	{
-		while (h != 0)
-		{
-			printf("%s", h->str);
-			printf("%d", h->len);
-			node++;
-			h = h->next;
-		}
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		nodes++;
+		h = h->next;
 	}
-	return (node);
+	return (nodes);
 }
 
