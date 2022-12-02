cf5f9e6183189b353159ba290c23b2bfe3722462
Henryssentamu <ssentamuhenry61@gmail.com>
2022-12-02 01:32:12 -0800

    fixed

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,15 +2,22 @@
 /**
  * list_len -> number of elements in a linked list_t list.
  * @h: head
+ * @newh: ponter
+ * @i: int
  * Return: number of elements in a linked list_t
  */
 size_t list_len(const list_t *h)
 {
 	size_t i = 0;
 
+	list_t *newh;
+
+	newh = malloc(sizeof(list_t));
+	h = newh;
+	
 	while (h != NULL)
 	{
-		h = h->next;
+		newh = newh->next;
 		i++;
 	}
 	return (i);
